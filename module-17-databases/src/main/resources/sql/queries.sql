6c3d8667420e407862750fe7faba7f58891df113
Yuliia Lysak <julia.m.lysak@gmail.com>
2022-02-22 20:45:15 +0200

    [task-5] Added trigger for students.updated_datetime

--- module-17-databases/src/main/resources/sql/queries.sql
+++ module-17-databases/src/main/resources/sql/queries.sql
@@ -29,3 +29,23 @@ FROM students st
          INNER JOIN exam_results er ON st.id = er.student_id
          INNER JOIN subjects su ON su.id = er.subject_id
 WHERE surname LIKE 'Greg%';
+
+
+-- 5. Add trigger that will update column updated_datetime to current date in case of updating any of student. (0.3 point)
+CREATE OR REPLACE FUNCTION upd_datetime()
+RETURNS TRIGGER AS $$
+   BEGIN
+       NEW.updated_datetime = now();
+       RETURN NEW;
+   END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER students_update_datetime BEFORE UPDATE
+    ON students FOR EACH ROW EXECUTE PROCEDURE upd_datetime();
+
+
+SELECT * FROM students WHERE id = 1;
+
+UPDATE students
+SET primary_skill = 'Health'
+WHERE id = 1;
