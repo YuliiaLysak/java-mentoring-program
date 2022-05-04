627f7905b3e737d4fbc3b625e531a0a5f7d8bf37
Yuliia Lysak <julia.m.lysak@gmail.com>
2022-05-05 00:22:00 +0300

    [module-24] Added 'apigateway' module (zuul)

---
+++ module-24-microservices/main-task/platform-services/apigateway/zuul-ui/src/main/java/edu/lysak/ui/filter/CustomZuulErrorApplication.java
@@ -0,0 +1,15 @@
+package edu.lysak.ui.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class CustomZuulErrorApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomZuulErrorApplication.class, args);
+    }
+}
